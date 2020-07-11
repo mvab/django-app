0f5adcee25338c2f2230853cf7e44226f615526e
Marina Vabistsevits <ny19205@bristol.ac.uk>
2020-07-11 16:41:02 +0100

    create Page model and set up Admin

--- mfdw_root/pages/admin.py
+++ mfdw_root/pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("title", "update_date") # what field to show in the tables column at the pages list
+    ordering = ("title",) # sort by
+    search_fields = ("title",) # search by
+
+
+
 
 # Register your models here.
+admin.site.register(Page, PageAdmin)
--- mfdw_root/pages/models.py
+++ mfdw_root/pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=60)
+    permalink = models.CharField(max_length=12, unique=True)
+    update_date = models.DateTimeField("Last Updated")
+    bodytext = models.TextField('Page Content', blank=True) # blank=T : create page without any content
+
+    def __str__(self):
+        return self.title
