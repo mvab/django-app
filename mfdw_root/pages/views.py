5ee2ebb30d272f9a5c3a49ee6fc49e8a3d451e87
Marina Vabistsevits <ny19205@bristol.ac.uk>
2020-07-12 19:44:54 +0100

    create dynamic pages and add navigation

--- mfdw_root/pages/views.py
+++ mfdw_root/pages/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from . models import Page
 #from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    #return HttpResponse("<h1> Website Homepage </h1>")
-    return render(request, "pages/page.html")
+#def index(request):
+#    #return HttpResponse("<h1> Website Homepage </h1>")
+#    return render(request, "pages/page.html")
+
+
+def index(request, pagename):
+    pagename = "/" + pagename # this is needed because Dj removes / from end of URLs, so need to add this to point to the root
+    # load Page model
+    pg = Page.objects.get(permalink=pagename)
+    # topolate dictionary of items to pass to the template
+    context = {
+        "title" : pg.title,
+        "content" : pg.bodytext,
+        "last_updated" : pg.update_date,
+        "page_list" : Page.objects.all(),
+    }
+    # assert False # enters the error page view
+    return render(request, "pages/page.html", context)
