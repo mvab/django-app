a178915ff2375c1897d254a04af028f87e0e83a1
Marina Vabistsevits <ny19205@bristol.ac.uk>
2020-07-12 17:24:10 +0100

    set up a basic template for site and app

--- mfdw_root/pages/views.py
+++ mfdw_root/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Website Homepage </h1>")
+    #return HttpResponse("<h1> Website Homepage </h1>")
+    return render(request, "pages/page.html")
